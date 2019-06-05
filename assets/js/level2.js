e2429b0cf5177f1bee64e42a190fcbf63e184094
Michael Leese <illumni@outlook.com>
2019-06-05 16:05:02 +0000

    remove show grid on level2

--- assets/js/level2.js
+++ assets/js/level2.js
@@ -43,7 +43,6 @@ class Level2 extends Phaser.Scene { //creates a scene in the Phaser Object calle
 
         //CREATE GRID
         this.aGrid = new AlignGrid({ scene: this, rows: 11, cols: 11 }); //add grid to screen for scaling and positioning
-        this.aGrid.showNumbers(); //show grid
         //END GRID
 
         //create sfx
